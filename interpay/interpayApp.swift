e2ad28d6042f35d09bca66f89650f15439ed342d
DiegoOLopez <92211663+DiegoOLopez@users.noreply.github.com>
2025-11-08 14:37:45 -0600

    Initial Commit

---
+++ interpay/interpayApp.swift
@@ -0,0 +1,17 @@
+//
+//  interpayApp.swift
+//  interpay
+//
+//  Created by Diego Obed on 08/11/25.
+//
+
+import SwiftUI
+
+@main
+struct interpayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
